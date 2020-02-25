2a8288ce56af24744ef22bdf9e9fd7b9818d9275
Jake Coker <jake.coker91@gmail.com>
2020-02-25 13:47:56 -0800

    moved chrome driver instantiation

--- main.py
+++ main.py
@@ -11,57 +11,58 @@ passwordMain = getpass('Password: ')
 FM_Export = 'FM_ALL.csv'
 computerList = getFileMakerDatabase(FM_Export)
 
-while True:
-    cpuNumber = input('CPU NUMBER: ')
-    chrome_browser = webdriver.Chrome('./chromedriver.exe')
-    computerName = getComputerName(computerList, cpuNumber)
+if __name__ == '__main__':
+    while True:
+        cpuNumber = input('CPU NUMBER: ')
+        computerName = getComputerName(computerList, cpuNumber)
 
-    chrome_browser.get('https://10.37.0.228/zenworks/jsp/index.jsp?pageid=deviceList')
-    button = chrome_browser.find_element_by_id('details-button')
-    button.click()
-    button = chrome_browser.find_element_by_id('proceed-link')
-    button.click()
+        chrome_browser = webdriver.Chrome('./chromedriver.exe')
+        chrome_browser.get('https://10.37.0.228/zenworks/jsp/index.jsp?pageid=deviceList')
+        button = chrome_browser.find_element_by_id('details-button')
+        button.click()
+        button = chrome_browser.find_element_by_id('proceed-link')
+        button.click()
 
-    username = chrome_browser.find_element_by_id('username')
-    username.clear()
-    username.send_keys(usernameMain)
+        username = chrome_browser.find_element_by_id('username')
+        username.clear()
+        username.send_keys(usernameMain)
 
-    password = chrome_browser.find_element_by_id('password')
-    password.clear()
-    password.send_keys(passwordMain)
+        password = chrome_browser.find_element_by_id('password')
+        password.clear()
+        password.send_keys(passwordMain)
 
-    button = chrome_browser.find_element_by_id('loginButton')
-    button.click()
+        button = chrome_browser.find_element_by_id('loginButton')
+        button.click()
 
-    computerNameInput = chrome_browser.find_element_by_id('mainPage_deviceList_advancedSearch_defaultSearch')
-    computerNameInput.clear()
-    computerNameInput.send_keys(computerName)
-    button = chrome_browser.find_element_by_id('mainPage_deviceList_advancedSearch_searchButton')
-    button.click()
+        computerNameInput = chrome_browser.find_element_by_id('mainPage_deviceList_advancedSearch_defaultSearch')
+        computerNameInput.clear()
+        computerNameInput.send_keys(computerName)
+        button = chrome_browser.find_element_by_id('mainPage_deviceList_advancedSearch_searchButton')
+        button.click()
 
-    button = chrome_browser.find_element_by_xpath('//*[@title="View Details"]')
-    button.click()
+        button = chrome_browser.find_element_by_xpath('//*[@title="View Details"]')
+        button.click()
 
-    button = chrome_browser.find_element_by_xpath('//*[contains(text(), "Remote Control")]')
-    button.click()
+        button = chrome_browser.find_element_by_xpath('//*[contains(text(), "Remote Control")]')
+        button.click()
 
-    select = Select(chrome_browser.find_element_by_id('mainPage_cmdHandlers_remoteControlWksDet_rmPopup_agentList'))
-    select.select_by_index(1)
+        select = Select(chrome_browser.find_element_by_id('mainPage_cmdHandlers_remoteControlWksDet_rmPopup_agentList'))
+        select.select_by_index(1)
 
-    time.sleep(1)
+        time.sleep(1)
 
-    button = chrome_browser.find_element_by_id('mainPage_cmdHandlers_remoteControlWksDet_rmPopup_okButton')
-    button.click()
+        button = chrome_browser.find_element_by_id('mainPage_cmdHandlers_remoteControlWksDet_rmPopup_okButton')
+        button.click()
 
-    time.sleep(1)
-
-
-
-    actions = ActionChains(chrome_browser)
-    actions.send_keys(Keys.ENTER)
-    actions.perform()
+        time.sleep(1)
 
 
-    # alertPrompt = chrome_browser.switch_to_alert()
-    # alertPrompt.accept()
-    chrome_browser.quit()
+
+        actions = ActionChains(chrome_browser)
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+
+
+        # alertPrompt = chrome_browser.switch_to_alert()
+        # alertPrompt.accept()
+        chrome_browser.quit()
